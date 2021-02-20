950ba7e09694a2a9b9682d28b807635e365d0bf0
Ramanathan Varadharajan <ramanathan.varadharajan@wur.nl>
2021-02-20 13:12:01 +0100

    Function to get rigidities from S(q)

--- datafit.m
+++ datafit.m
@@ -1,3 +1,7 @@
+% Linear Fitting function to get the rigidites. 
+% slope will give the mean bending rigidity
+% intercept will provide the interfacial tension
+
 function [slope intercept] = datafit(q,hq,start,ends)
 qsquare=q(start:ends).*q(start:ends);
 y=1./(qsquare.*hq(start:ends));
